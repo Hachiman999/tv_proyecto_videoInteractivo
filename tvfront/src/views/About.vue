67159b14d129a0a2dfcd6407b0fc9368f4ef4ca2
Hachiman999 <30505008+Hachiman999@users.noreply.github.com>
2020-10-20 12:29:32 -0500

    se agrego in template

---
+++ tvfront/src/views/About.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+      <h1>{{ titulo }}</h1>
+  <p>
+      nombre
+      <input type="text" name="" id="" v-model="usuario.name">
+  </p>
+  <p>
+      comentario 
+     <textarea name="" id="" cols="30" rows="10" v-model="usuario.comentario"></textarea>
+  </p>
+  <button type="submit">enviar</button>
+ 
+  </div>
+</template>
+<script>
+export default {
+  name: 'EnvioData',
+  props: {
+    titulo: String
+  },
+  data() {
+    return {
+      usuario:{
+          name:'',
+          comentario:''
+      }
+    }
+  }
+}
+</script>
