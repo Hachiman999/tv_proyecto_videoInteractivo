bc1d18b4fb5b61b5f5f9b482016661870cbd1f7f
Hachiman999 <30505008+Hachiman999@users.noreply.github.com>
2020-11-03 13:59:09 -0500

    se inserto el primer video
    video

--- tvfrontv2/src/components/index.js
+++ tvfrontv2/src/components/index.js
@@ -1,27 +1,54 @@
 import React, { Component } from "react";
-
+import video1 from '../assets/finalllegandoacasa.mp4'
+import video2 from '../assets/pruebaconanimate.mp4'
 class App extends Component{
 
 constructor(props){
     super(props)
-    this.state={tiempo :0, video:1}
+    this.state={tiempo :0, video:1 ,src : video1}
     this.metodoq = this.metodoq.bind(this); 
     this.metodox = this.metodox.bind(this); 
+    this.comportamientoVideo= this.comportamientoVideo.bind(this); 
 }
    
 componentDidMount() {
-      // VARIABLES VIDEO 1
+           
+        this.comportamientoVideo(this.mount)
+       
+
+}
+comportamientoVideo(body){
+    this.video = document.createElement('video');
+    this.video.src = video1; 
+    body.appendChild(this.video); 
+    var {video, tiempo}= this.state; 
+   this.video.play(); 
+    this.video.addEventListener("timeupdate",function(ev){
+        tiempo = ev.target.currentTime;
+        if(tiempo >= 11 && video ===1){
+            ev.target.currentTime=8; 
+               // VARIABLES VIDEO 1
         var btn1 = document.createElement("BUTTON");
         btn1.id = "boton1"
         btn1.appendChild(document.createTextNode("Aceptar"))
+        body.appendChild( btn1); 
+        
+     btn1.onClick=()=>{
+        console.log(video); 
+     }
+      
 		
         var btn2 = document.createElement("BUTTON");
         btn2.id = "boton2"
         btn2.appendChild(document.createTextNode("rechazar"))
-        
-        
-        this.mount.appendChild(btn1)
+        }
+       // console.log(tiempo)
+        if(tiempo >= 11 && video ===2){
+            ev.target.currentTime=8; 
+        }
 
+       },true);
+  
 }
    
     metodoq(){
@@ -59,3 +86,49 @@ componentDidMount() {
 }
 
 export default App;
+
+/*
+addEventListener("click", ()=>{}, false);
+*/
+
+/*
+         ,--"""",--.__,---[],-------._         
+       ,"   __,'            \         \--""""""==;-
+     ," _,-"  "/---.___     \       ___\   ,-'',"
+    /,-'      / ;. ,.--'-.__\  _,-"" ,| `,'   /
+   /``""""-._/,-|:\       []\,' ```-/:;-. `. /
+             `  ;:::      ||       /:,;  `-.\
+                =.,'__,---||-.____',.=
+                =(:\_     ||__    ):)=
+               ,"::::`----||::`--':::"._
+             ,':::::::::::||::::::::::::'.
+    .__     ;:::.-.:::::__||___:::::.-.:::\     __,
+       """-;:::( O )::::>_|| _<::::( O )::::-"""
+   =======;:::::`-`:::::::||':::::::`-`:::::\=======
+    ,--"";:::_____________||______________::::""----.          , ,
+         ; ::`._(    |    |||     |   )_,'::::\_,,,,,,,,,,____/,'_,
+       ,;    :::`--._|____[]|_____|_.-'::::::::::::::::::::::::);_
+      ;/ /      :::::::::,||,:::::::::::::::::::::::::::::::::::/
+     /; ``''''----------/,'/,__,,,,,____:::::::::::::::::::::,"
+     ;/                :);/|_;| ,--.. . ```-.:::::::::::::_,"
+    /;                :::):__,'//""\\. ,--.. \:::,:::::_,"
+   ;/              :::::/ . . . . . . //""\\. \::":__,"
+   ;/          :::::::,' . . . . . . . . . . .:`::\
+   ';      :::::::__,'. ,--.. . .,--. . . . . .:`::`
+   ';   __,..--'''-. . //""\\. .//""\\ . ,--.. :`:::`
+   ;    /  \\ .//""\\ . . . . . . . . . //""\\. :`::`
+   ;   /       . . . . . . . . . . . . . . . . .:`::`
+   ;   (          . . . . . . . . . . . . . . . ;:::`
+   ,:  ;,            . . . . . . . . . . . . . ;':::`
+   ,:  ;,             . . . . . . . . . . . . .;`:::
+   ,:   ;,             . . . . . . . . . . . . ;`::;`
+    ,:  ;             . . . . . . . . . . . . ;':::;`
+     :   ;             . . . . . . . . . . . ,':::;
+      :   '.          . . . . . . . .. . . .,':::;`
+       :    `.       . . . . . . . . . . . ;::::;`
+        '.    `-.   . . . . . . . . . . ,-'::::;
+          `:_    ``--..___________..--'':::::;'`
+             `._::,.:,.:,:_ctr_:,:,.::,.:_;'`
+________________`"\/"\/\/'""""`\/"\/""\/"____________________________
+
+*/
